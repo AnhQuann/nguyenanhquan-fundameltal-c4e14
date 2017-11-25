1e21ba4db072a60244c220ee88424db690f88b5a
AnhQuann <anhquan19051905@gmail.com>
2017-11-26 01:54:12 +0700

    initial commit

---
+++ session02/homework/serious_4a_4d.py
@@ -0,0 +1,7 @@
+row = int(input('Enter the number of rows:'))
+column = int(input('Enter the number of columns:'))
+
+for i in range(1, row+1):
+    for i in range(1, column+1):
+        print('* ', end='')
+    print()
--- session02/homework/serious_4b_4e.py
+++ session02/homework/serious_4b_4e.py
@@ -1,11 +1,6 @@
 row = int(input('Enter the number of rows:'))
 column = int(input('Enter the number of columns:'))
 
-# for i in range(1, row+1):
-#     for i in range(1, column+1):
-#         print('* ', end='')
-#     print()
-
 for i in range(1, row+1):
     if (i%2==0):
         for i in range(1, column+1):
